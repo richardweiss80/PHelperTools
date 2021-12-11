b6f8d84c1e43fcd425ce08181d2dad719fdd94e7
Richard <9610284+richardweiss80@users.noreply.github.com>
2021-12-11 21:44:01 +0100

    Update HashDLLExports.py
    some improvements, but the hashing algorithms not implemented yet. please feel free to contribute

--- HashDLLExport/HashDLLExports.py
+++ HashDLLExport/HashDLLExports.py
@@ -6,32 +6,84 @@
 
 import sys
 import re
+import argparse
+import textwrap
+from malduck import UInt32
 
-threatactor = "cycldek"
 
-def hash_func(data):
+threatactor = ["cycldek", "emotet_new", "FNV_1a", "SF_reflectiveDLL"]
+
+def hash_func(provider, data):
+    return globals()[provider](data)
+
+
+def cycldek(data):
+    # filehash: 
     # Cyckdek Windows Functions Hashing Algorithm
     hash = 0
     for char in data:
         hash = (((hash >> 7) & 0xFFFFFFFF) | ((hash << 25) & 0xFFFFFFFF)) + ord(char)
-    result[data] = hex(hash)
+    return hex(hash)
+
+def emotet_new(data):
+    # filehash: 
+    # Used for the new emotet version late 2021
+    hash = UInt32(0)
+    # hash = 0
+    for char in data:
+        # hash = (ord(char.lower()) + hash * 0x1003f) & 0xffffffff
+        hash = (hash << 16) + (hash << 6) + ord(char) - hash
+    return hex(hash ^ 0x326E19FC) # 0x6C60cfb2 Test: 5BCF9C13
+
+def FNV_1a(data):
+    # filehash:
+    hash = 0
+    # ToDo
+    return hex(hash)
+
+def SF_reflectiveDLL(data):
+    #filehash:
+    hash = 0
+    # ToDo
+    return hex(hash)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: <toolname> <filename>")
-
+    parser = argparse.ArgumentParser(
+        prog='HashDLLExport',
+        description="Processes dumpbin export list",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''\
+            additional information:
+                Included Hashing Functions:
+                %s
+            ''')%(threatactor))
+    parser.add_argument('-i', '--input', dest='input', required=True, help='the dumpbin exports output file as input') 
+    parser.add_argument('--hashing', required=True, choices = threatactor, help='the hashing routine name')
+    parser.add_argument('-o', '--output', dest='output', help='the output file')
+    args = parser.parse_args()
+    
+    dllfile = ""
     regexp = re.compile(r"^\s+\d{1,4}\s+[A-Fa-f0-9]{1,3}\s+([A-Fa-f0-9]{8}\s+\w+|\w+\s+\(.*\))")
     count = 0
     result = {}
     
-    with open(sys.argv[1], "r", encoding="utf-16") as file_object:
+    with open(args.input, "r", encoding="utf-16") as file_object:
         for line in file_object:
+            if "Dump of file" in line:
+                dllfile = line.split("\\")[-1].strip()
             if regexp.match(line):
                 count = count + 1
-                hash_func((re.findall(r'[a-zA-Z_]{4,}', line))[0])
+                data = line[26:].split(" ")[0].strip()
+                result[data] = hash_func(args.hashing, data)
 
     print(count)
-    with open("%s_%s_hashed" %(sys.argv[1], threatactor) , "w", encoding="utf-16") as file_object:
+
+    if  not args.output:
+        output_file = "%s_%s_%s_hashed.txt" %(dllfile, hash_func(args.hashing, dllfile), args.hashing)
+    else: 
+        output_file = args.output  
+
+    with open(output_file , "w", encoding="utf-16") as file_object:
         for key in result.keys():
             file_object.write("%s, %s\n" % (key, result[key]))
