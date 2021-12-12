8af83e40fe6aebd677d1310cb4cc6d85fcbd5ad2
Richard <9610284+richardweiss80@users.noreply.github.com>
2021-12-12 23:16:01 +0100

    Update HashDLLExports.py
    added fnv-1a 64 bit and the list parameter

--- HashDLLExport/HashDLLExports.py
+++ HashDLLExport/HashDLLExports.py
@@ -3,13 +3,14 @@
 # add the hash func.
 
 # could result in false representation, if the RVA result contains [A-Z]{4,}
+# for using a list of strings (seperated by newline) to hash, please use the argument --list
 
 import sys
 import re
 import argparse
 import textwrap
 
-threatactor = ["cycldek", "emotet_new", "FNV_1a", "SF_reflectiveDLL"]
+threatactor = ["cycldek", "emotet_new", "solarwinds_FNV_1a", "SF_reflectiveDLL"]
 
 def hash_func(provider, data):
     return globals()[provider](data)
@@ -24,26 +25,35 @@ def cycldek(data):
     return hex(hash)
 
 def emotet_new(data):
-    # filehash: ba758c64519be23b5abe7991b71cdcece30525f14e225f2fa07bbffdf406e539
+    # filehash: 
     # Used for the new emotet version late 2021
-    # For Hashing the basedllname another value is used: 0x326E19FC
-    hash = 0
+    hash = UInt32(0)
+    # hash = 0
     for char in data:
-        hash = ((hash << 16) + (hash << 6) + ord(char) - hash) & 0xFFFFFFFF
-    return hex(hash ^ 0x1E5C48DE) # Figure out the correst XOR_Key specific to the sample
+        # hash = (ord(char.lower()) + hash * 0x1003f) & 0xffffffff
+        hash = (hash << 16) + (hash << 6) + ord(char) - hash
+    return hex(hash ^ 0x1E5C48DE) # 0x6C60cfb2 Test: 5BCF9C13
 
-def FNV_1a(data):
+def FNV_1a64(data):
+    hash = 0xcbf29ce484222325
+    fnv_64_prime = 0x100000001b3
+    uint64_max = 2 ** 64
+    for char in data:
+        hash = hash ^ ord(char.lower())
+        hash = (hash * fnv_64_prime) % uint64_max
+    return hash
+
+def solarwinds_FNV_1a(data):
     # filehash:
-    hash = 0
-    # ToDo
-    return hex(hash)
+    # reference: https://www.mandiant.com/resources/evasive-attacker-leverages-solarwinds-supply-chain-compromises-with-sunburst-backdoor
+    # description: FNV-1A 64 bit XOR 6605813339339102567
+    return hex(FNV_1a64(data) ^ 6605813339339102567)
 
 def SF_reflectiveDLL(data):
     #filehash:
     hash = 0
     # ToDo
     return hex(hash)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -55,8 +65,9 @@ if __name__ == "__main__":
                 Included Hashing Functions:
                 %s
             ''')%(threatactor))
-    parser.add_argument('-i', '--input', dest='input', required=True, help='the dumpbin exports output file as input') 
-    parser.add_argument('--hashing', required=True, choices = threatactor, help='the hashing routine name')
+    parser.add_argument('-i', '--input', dest='input', required=True, help='the dumpbin exports output file as input')
+    parser.add_argument('--list', help='use a new line seperated list instead of dumbin.exe-export', action='store_true')
+    parser.add_argument('--hashing', required=True, choices=threatactor, help='the hashing routine name')
     parser.add_argument('-o', '--output', dest='output', help='the output file')
     args = parser.parse_args()
     
@@ -65,19 +76,28 @@ if __name__ == "__main__":
     count = 0
     result = {}
     
-    with open(args.input, "r", encoding="utf-16") as file_object:
-        for line in file_object:
-            if "Dump of file" in line:
-                dllfile = line.split("\\")[-1].strip()
-            if regexp.match(line):
+    if not args.list:
+        with open(args.input, "r", encoding="utf-16") as file_object:
+            for line in file_object:
+                if "Dump of file" in line:
+                    dllfile = line.split("\\")[-1].strip()
+                if regexp.match(line):
+                    count = count + 1
+                    data = line[26:].split(" ")[0].strip()
+                    result[data] = hash_func(args.hashing, data)
+    else:
+        with open(args.input, "r") as file_object:
+            for line in file_object:
                 count = count + 1
-                data = line[26:].split(" ")[0].strip()
-                result[data] = hash_func(args.hashing, data)
+                result[line] = hash_func(args.hashing, line)
 
     print(count)
 
     if  not args.output:
-        output_file = "%s_%s_%s_hashed.txt" %(dllfile, hash_func(args.hashing, dllfile), args.hashing)
+        if not args.list:
+            output_file = "%s_%s_%s_hashed.txt" % (dllfile, hash_func(args.hashing, dllfile), args.hashing)
+        else:
+            output_file = "%s_hashed.txt" % (args.input)
     else: 
         output_file = args.output  
 
